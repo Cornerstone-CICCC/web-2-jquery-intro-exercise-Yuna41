a8d79b8004fddfc6d00feb8a0c8f1d908e3e5ce8
Yuna41 <uncnprs@gmail.com>
2025-08-05 21:02:44 -0700

    Complete jquery intro exercise

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 $(function() {
-  // YOUR CODE HERE
+  // exercise 1
+  $('#toggleButton').on('click', function(){
+    $('#myParagraph').toggle()
+  })
+
+  // exercise 2
+  $('#colorButton').on('click', function(){
+    $(this).next().css('background', 'yellow')
+  })
+
+  //exercise 3
+  $('#addClassButton').on('click', function(){
+    $('#classDiv').addClass('newClass')
+  })
+  $('#removeClassButton').on('click', function(){
+    $('#classDiv').removeClass('newClass')
+  })
+
+  // exercise 4
+  $('#fadeInButton').on('click', function(){
+    $('#fadeDiv').fadeIn()
+  })
+  $('#fadeOutButton').on('click', function(){
+    $('#fadeDiv').fadeOut()
+  })
+
+  // exercise 5
+  $('#slideUpButton').on('click', function(){
+    $('#slideDiv').slideUp()
+  })
+  $('#slideDownButton').on('click', function(){
+    $('#slideDiv').slideDown()
+  })
 })
